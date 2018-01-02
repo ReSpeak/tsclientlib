5a63c91115a5e15e0dc670127edf17ffc5f0def5
Splamy <splamyn@gmail.com>
2018-01-02 23:17:30 +0100

    Implemented notifications

--- tsproto-commands/src/structs.rs
+++ tsproto-commands/src/structs.rs
@@ -2,10 +2,6 @@ use ::*;
 use errors::Error;
 use permissions::PermissionId;
 
-pub trait Notification {
-    fn get_notification_type() -> NotificationType;
-}
-
 pub trait Response {
     fn get_return_code(&self) -> &str;
     fn set_return_code(&mut self, return_code: String);
