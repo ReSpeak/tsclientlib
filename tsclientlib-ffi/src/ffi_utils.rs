bf6db3c22d8d7710fa9ee9fd47a4c10430160360
Flakebi <flakebi@t-online.de>
2019-03-26 01:12:09 +0100

    Prefix more tscl and return futures in tsclientlib-ffi

---
+++ tsclientlib-ffi/src/ffi_utils.rs
@@ -0,0 +1,63 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+use futures::Future;
+use slog::warn;
+
+use crate::{Error, Event, EVENTS, FutureHandle, LOGGER, Result, RUNTIME};
+
+/// Allocates a `CString` which has to be freed by the receiver afterwards.
+///
+/// Returns a null pointer if the string contains a 0 character.
+#[inline]
+fn str_to_ffi(s: &str) -> *mut c_char {
+	CString::new(s).map(|s| s.into_raw()).unwrap_or(std::ptr::null_mut())
+}
+
+// Take a reference to get the lifetime of the resulting `str`.
+#[inline]
+pub(crate) fn ffi_to_str(s: &*const c_char) -> Result<&str> {
+	unsafe { CStr::from_ptr(*s) }.to_str().map_err(Error::from)
+}
+
+pub trait ToFfiStringExt {
+	fn ffi(&self) -> *mut c_char;
+}
+
+impl ToFfiStringExt for Error {
+	fn ffi(&self) -> *mut c_char {
+		// TODO Use translatable string
+		str_to_ffi(&self.to_string())
+	}
+}
+
+impl<T> ToFfiStringExt for Result<T> {
+	fn ffi(&self) -> *mut c_char {
+		self.as_ref().err().map(ToFfiStringExt::ffi)
+			.unwrap_or(std::ptr::null_mut())
+	}
+}
+
+impl ToFfiStringExt for str {
+	fn ffi(&self) -> *mut c_char {
+		str_to_ffi(self)
+	}
+}
+
+pub(crate) trait ToFfiFutureExt {
+	fn ffi(self) -> FutureHandle;
+}
+
+impl<F: Future<Item=(), Error=Error> + Send + 'static> ToFfiFutureExt for F {
+	fn ffi(self) -> FutureHandle {
+		let h = FutureHandle::next_free();
+		RUNTIME.executor().spawn(self.then(move |r| {
+			if let Err(e) = &r {
+				warn!(LOGGER, "Future exited with error"; "error" => ?e);
+				EVENTS.0.send(Event::FutureFinished(h, r)).unwrap();
+			}
+			Ok(())
+		}));
+		h
+	}
+}
--- tsclientlib-ffi/src/lib.rs
+++ tsclientlib-ffi/src/lib.rs
@@ -1,26 +1,37 @@
-#[macro_use]
-extern crate failure;
-
 use std::ffi::{CStr, CString};
 use std::fmt;
 use std::os::raw::c_char;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 use chashmap::CHashMap;
 use crossbeam::channel;
-use futures::{future, Future, Sink, StartSend, Async, AsyncSink, Poll};
+use derive_more::From;
+use failure::{format_err, Fail, ResultExt};
+use futures::{future, Future};
+#[cfg(feature = "audio")]
+use futures::{Sink, StartSend, Async, AsyncSink, Poll};
+use futures::sync::oneshot;
 use lazy_static::lazy_static;
 use num::ToPrimitive;
-use parking_lot::{Mutex, RwLock};
+use parking_lot::Mutex;
+#[cfg(feature = "audio")]
+use parking_lot::RwLock;
 use slog::{error, o, Drain, Logger};
 use tsclientlib::{
 	ChannelId, ClientId, ConnectOptions, Connection, ServerGroupId,
 };
 use tsclientlib::events::Event as LibEvent;
+#[cfg(feature = "audio")]
 use tsproto::packets::OutPacket;
 #[cfg(feature = "audio")]
 use tsproto_audio::{audio_to_ts, ts_to_audio};
 
-//type Result<T> = std::result::Result<T, tsclientlib::Error>;
+type Result<T> = std::result::Result<T, Error>;
+type BoxFuture<T> = Box<Future<Item=T, Error=Error> + Send + 'static>;
+
+mod ffi_utils;
+
+use ffi_utils::*;
 
 lazy_static! {
 	static ref LOGGER: Logger = {
@@ -38,8 +49,24 @@ lazy_static! {
 		.unwrap();
 	static ref FIRST_FREE_CON_ID: Mutex<ConnectionId> =
 		Mutex::new(ConnectionId(0));
+	/// A list of all connections that were started but are not yet connected.
+	///
+	/// By sending a message to the channel, the connection can be canceled.
+	static ref CONNECTING: CHashMap<ConnectionId, oneshot::Sender<()>> =
+		CHashMap::new();
+	/// All currently open connections.
 	static ref CONNECTIONS: CHashMap<ConnectionId, Connection> =
 		CHashMap::new();
+
+	/// To obtain a unique id for a future, we just increment this counter.
+	///
+	/// Use `FutureHandle::next_free` to obtain a handle.
+	///
+	/// In theory, this could lead to two futures using the same counter if we
+	/// wrap arount in between. We ignore this case because futures are likely
+	/// short lived and a user does not spawn 4 billion futures while another
+	/// future is still running.
+	static ref NEXT_FUTURE_HANDLE: AtomicU32 = AtomicU32::new(0);
 
 	/// Transfer events to whoever is listening on the `next_event` method.
 	static ref EVENTS: (channel::Sender<Event>, channel::Receiver<Event>) =
@@ -66,34 +93,55 @@ lazy_static! {
 
 include!(concat!(env!("OUT_DIR"), "/book_ffi.rs"));
 
+// **** FFI types ****
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct ConnectionId(u32);
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+#[repr(transparent)]
+pub struct FutureHandle(u32);
+
 #[repr(u32)]
+#[derive(Clone, Copy, Debug)]
 pub enum EventType {
 	ConnectionAdded,
 	ConnectionRemoved,
+	FutureFinished,
 	Message,
-}
-
-enum Event {
-	ConnectionAdded(ConnectionId),
-	ConnectionRemoved(ConnectionId),
-	Event(ConnectionId, LibEvent),
 }
 
 #[repr(C)]
 pub struct FfiEvent {
 	content: FfiEventUnion,
 	typ: EventType,
+	connection: ConnectionId,
+	has_connection_id: bool,
 }
 
 #[repr(C)]
 pub union FfiEventUnion {
-	connection_added: ConnectionId,
-	connection_removed: ConnectionId,
+	empty: (),
+	future_result: FfiFutureResult,
 	message: EventMsg,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct FfiFutureResult {
+	/// Is `null` if the future was successful.
+	error: *mut c_char,
+	handle: FutureHandle,
+}
+
+#[derive(Clone, Copy, Debug)]
+#[repr(C)]
+pub struct Invoker {
+	name: *mut c_char,
+	/// The uid may be null.
+	uid: *mut c_char,
+	id: u16,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -101,7 +149,7 @@ pub union FfiEventUnion {
 pub struct EventMsg {
 	connection: ConnectionId,
 	target_type: MessageTarget,
-	invoker: u16,
+	invoker: Invoker,
 	message: *mut c_char,
 }
 
@@ -114,13 +162,86 @@ pub enum MessageTarget {
 	Poke,
 }
 
+// **** Non FFI types ****
+
+enum Event {
+	ConnectionAdded(ConnectionId),
+	ConnectionRemoved(ConnectionId),
+	FutureFinished(FutureHandle, Result<()>),
+	Event(ConnectionId, LibEvent),
+}
+
+#[derive(Fail, Debug, From)]
+enum Error {
+	#[fail(display = "{}", _0)]
+	Tsclientlib(#[cause] tsclientlib::Error),
+	#[fail(display = "{}", _0)]
+	Utf8(#[cause] std::str::Utf8Error),
+
+	#[fail(display = "{}", _0)]
+	Other(#[cause] failure::Compat<failure::Error>),
+}
+
+// **** End types ****
+
+impl From<failure::Error> for Error {
+	fn from(e: failure::Error) -> Self {
+		let r: std::result::Result<(), _> = Err(e);
+		Error::Other(r.compat().unwrap_err())
+	}
+}
+
 impl Event {
 	fn get_type(&self) -> EventType {
 		match self {
 			Event::ConnectionAdded(_) => EventType::ConnectionAdded,
 			Event::ConnectionRemoved(_) => EventType::ConnectionRemoved,
+			Event::FutureFinished(_, _) => EventType::FutureFinished,
 			Event::Event(_, LibEvent::Message { .. }) => EventType::Message,
 			Event::Event(_, _) => unimplemented!("Events apart from message are not yet implemented"),
+		}
+	}
+
+	fn get_id(&self) -> Option<ConnectionId> {
+		match self {
+			Event::ConnectionAdded(id) => Some(*id),
+			Event::ConnectionRemoved(id) => Some(*id),
+			Event::FutureFinished(_, _) => None,
+			Event::Event(id, _) => Some(*id),
+		}
+	}
+}
+
+impl Into<FfiEvent> for Event {
+	fn into(self) -> FfiEvent {
+		let typ = self.get_type();
+		let id = self.get_id();
+		FfiEvent {
+			content: match self {
+				Event::FutureFinished(handle, r) => FfiEventUnion {
+					future_result: FfiFutureResult {
+						error: r.ffi(),
+						handle,
+					}
+				},
+				Event::Event(id, LibEvent::Message { from, invoker, message }) => FfiEventUnion {
+					message: EventMsg {
+						connection: id,
+						target_type: from.into(),
+						invoker: Invoker {
+							name: invoker.name.ffi(),
+							uid: invoker.uid.map(|uid| uid.0.ffi()).unwrap_or(std::ptr::null_mut()),
+							id: invoker.id.0,
+						},
+						message: message.ffi(),
+					}
+				},
+				Event::Event(_, _) => unimplemented!("Events apart from message are not yet implemented"),
+				_ => FfiEventUnion { empty: () },
+			},
+			typ,
+			connection: id.unwrap_or(ConnectionId(0)),
+			has_connection_id: id.is_some(),
 		}
 	}
 }
@@ -327,14 +448,22 @@ impl<'a> ConnectionMutExt<'a> for tsclientlib::data::ConnectionMut<'a> {
 	}
 }
 
+impl FutureHandle {
+	fn next_free() -> Self {
+		let id = NEXT_FUTURE_HANDLE.fetch_add(1, Ordering::Relaxed);
+		Self(id)
+	}
+}
+
 // TODO On future errors, send event
 
 impl ConnectionId {
-	fn next_free() -> Self {
-		let mut next_free = FIRST_FREE_CON_ID.lock();
+	/// This function should be called on a locked `FIRST_FREE_CON_ID`.
+	fn next_free(next_free: &mut Self) -> Self {
 		let res = *next_free;
 		let mut next = res.0 + 1;
-		while CONNECTIONS.contains_key(&ConnectionId(next)) {
+		while CONNECTIONS.contains_key(&ConnectionId(next))
+			|| CONNECTING.contains_key(&ConnectionId(next)) {
 			next += 1;
 		}
 		*next_free = ConnectionId(next);
@@ -363,103 +492,151 @@ fn remove_connection(con_id: ConnectionId) {
 		T2A_PIPES.remove(&con_id);
 	}
 
-	CONNECTIONS.remove(&con_id);
-	con_id.mark_free();
-	EVENTS.0.send(Event::ConnectionRemoved(con_id)).unwrap();
+	let removed;
+	// Cancel connection if it is connecting but not yet ready
+	// Important: Test first CONNECTING and then CONNECTIONS because they get
+	// first inserted into CONNECTIONS and then removed from CONNECTING so if a
+	// connetcion is not in CONNECTING, it has to be in CONNECTIONS.
+	if let Some(con) = CONNECTING.remove(&con_id) {
+		con.send(()).unwrap();
+		removed = true;
+	} else {
+		// Connection does not exist if it is not in CONNECTIONS
+		removed = CONNECTIONS.remove(&con_id).is_some();
+	}
+
+	if removed {
+		con_id.mark_free();
+		EVENTS.0.send(Event::ConnectionRemoved(con_id)).unwrap();
+	}
 }
 
 #[no_mangle]
-pub extern "C" fn tscl_connect(address: *const c_char) -> ConnectionId {
-	let address = unsafe { CStr::from_ptr(address) };
-	let options = ConnectOptions::new(address.to_str().unwrap())
+pub unsafe extern "C" fn tscl_connect(
+	address: *const c_char,
+	con_id: *mut ConnectionId,
+) -> FutureHandle {
+	let res = connect(ffi_to_str(&address).unwrap());
+	*con_id = res.0;
+	res.1.ffi()
+}
+
+fn connect(address: &str) -> (ConnectionId, impl Future<Item=(), Error=Error>) {
+	let options = ConnectOptions::new(address)
 		.logger(LOGGER.clone());
-	let con_id = ConnectionId::next_free();
+	let (send, recv) = oneshot::channel();
 
-	RUNTIME.executor().spawn(
-		future::lazy(move || {
-			let mut options = options;
-			#[cfg(feature = "audio")] {
-				// Create TeamSpeak to audio pipeline
-				match ts_to_audio::Pipeline::new(LOGGER.clone(),
-					RUNTIME.executor()) {
-					Ok(t2a_pipe) => {
-						let aph = t2a_pipe.create_packet_handler();
-						options = options.audio_packet_handler(aph);
-						T2A_PIPES.insert(con_id, t2a_pipe);
-					}
-					Err(e) => error!(LOGGER, "Failed to create t2a pipeline";
-						"error" => ?e),
+	// Lock until we inserted the connection
+	let mut next_free = FIRST_FREE_CON_ID.lock();
+	let con_id = ConnectionId::next_free(&mut *next_free);
+
+	// Insert into CONNECTING so it can be canceled
+	CONNECTING.insert(con_id, send);
+	drop(next_free);
+
+	// TODO Send the connection added event when the user can request the connection
+	// status.
+	//EVENTS.0.send(Event::ConnectionAdded(con_id)).unwrap();
+
+	let con_id2 = con_id;
+	(con_id, future::lazy(move || {
+		#[cfg(feature = "audio")]
+		let mut options = options;
+		#[cfg(feature = "audio")] {
+			// Create TeamSpeak to audio pipeline
+			match ts_to_audio::Pipeline::new(LOGGER.clone(),
+				RUNTIME.executor()) {
+				Ok(t2a_pipe) => {
+					let aph = t2a_pipe.create_packet_handler();
+					options = options.audio_packet_handler(aph);
+					T2A_PIPES.insert(con_id, t2a_pipe);
 				}
+				Err(e) => error!(LOGGER, "Failed to create t2a pipeline";
+					"error" => ?e),
 			}
-			Connection::new(options).map(move |con| {
-				// Or automatically try to reconnect.
-				con.add_on_disconnect(Box::new(move || {
-					remove_connection(con_id);
-				}));
-
-				con.add_on_event("tsclientlibffi".into(), Box::new(move |_, events| {
-					// TODO Send all at once? Remember the problem with getting
-					// ServerGroups one by one, so you never know when they are
-					// complete.
-					for e in events {
-						if let LibEvent::Message { .. } = e {
-							EVENTS.0.send(Event::Event(con_id, e.clone())).unwrap();
-						}
-					}
-				}));
-
-				#[cfg(feature = "audio")] {
-					// Create audio to TeamSpeak pipeline
-					if A2T_PIPE.read().is_none() {
-						let mut a2t_pipe = A2T_PIPE.write();
-						if a2t_pipe.is_none() {
-							match audio_to_ts::Pipeline::new(LOGGER.clone(),
-								CurrentAudioSink, RUNTIME.executor(), None) {
-								Ok(pipe) => {
-									*a2t_pipe = Some(pipe);
-								}
-								Err(e) => error!(LOGGER,
-									"Failed to create a2t pipeline";
-									"error" => ?e),
-							}
-
-							// Set new connection as default talking server
-							*CURRENT_AUDIO_SINK.lock() =
-								Some((con_id, Box::new(con.get_packet_sink())));
-						}
-					}
-				}
-
-				CONNECTIONS.insert(con_id, con);
-				EVENTS.0.send(Event::ConnectionAdded(con_id)).unwrap();
-			})
-			.map_err(move |e| {
-				error!(LOGGER, "Failed to connect"; "error" => %e);
+		}
+		Connection::new(options).map(move |con| {
+			// Or automatically try to reconnect (in tsclientlib)
+			con.add_on_disconnect(Box::new(move || {
 				remove_connection(con_id);
-			})
-		})
-	);
-	con_id
-}
+			}));
 
-#[no_mangle]
-pub extern "C" fn tscl_disconnect(con_id: ConnectionId) {
-	RUNTIME.executor().spawn(
-		future::lazy(move || {
-			if let Some(con) = CONNECTIONS.get(&con_id) {
-				con.clone().disconnect(None)
-			} else {
-				Box::new(future::err(
-					format_err!("Connection not found").into(),
-				))
+			con.add_on_event("tsclientlibffi".into(), Box::new(move |_, events| {
+				// TODO Send all at once? Remember the problem with getting
+				// ServerGroups one by one, so you never know when they are
+				// complete.
+				for e in events {
+					if let LibEvent::Message { .. } = e {
+						EVENTS.0.send(Event::Event(con_id, e.clone())).unwrap();
+					}
+				}
+			}));
+
+			#[cfg(feature = "audio")] {
+				// Create audio to TeamSpeak pipeline
+				if A2T_PIPE.read().is_none() {
+					let mut a2t_pipe = A2T_PIPE.write();
+					if a2t_pipe.is_none() {
+						match audio_to_ts::Pipeline::new(LOGGER.clone(),
+							CurrentAudioSink, RUNTIME.executor(), None) {
+							Ok(pipe) => {
+								*a2t_pipe = Some(pipe);
+							}
+							Err(e) => error!(LOGGER,
+								"Failed to create a2t pipeline";
+								"error" => ?e),
+						}
+
+						// Set new connection as default talking server
+						*CURRENT_AUDIO_SINK.lock() =
+							Some((con_id, Box::new(con.get_packet_sink())));
+					}
+				}
 			}
+
+			CONNECTIONS.insert(con_id, con);
+			EVENTS.0.send(Event::ConnectionAdded(con_id)).unwrap();
 		})
-		.map_err(|_| ()),
-	);
+		.map_err(move |e| {
+			error!(LOGGER, "Failed to connect"; "error" => %e);
+			remove_connection(con_id);
+			e
+		})
+	})
+	// Cancel
+	.select2(recv)
+	.then(move |_| {
+		// Remove from CONNECTING if it is still in there
+		CONNECTING.remove(&con_id2);
+		Ok(())
+	}))
 }
 
 #[no_mangle]
-pub extern "C" fn is_talking() -> bool {
+pub extern "C" fn tscl_disconnect(con_id: ConnectionId) -> FutureHandle {
+	disconnect(con_id).ffi()
+}
+
+fn disconnect(con_id: ConnectionId) -> BoxFuture<()> {
+	// Cancel connection if it is connecting but not yet ready
+	if let Some(con) = CONNECTING.remove(&con_id) {
+		con.send(()).unwrap();
+		return Box::new(future::ok(()));
+	}
+
+	if let Some(con) = CONNECTIONS.get(&con_id) {
+		Box::new(future::lazy(move || {
+			con.disconnect(None).from_err()
+		}))
+	} else {
+		Box::new(future::err(
+			format_err!("Connection not found").into(),
+		))
+	}
+}
+
+#[no_mangle]
+pub extern "C" fn tscl_is_talking() -> bool {
 	#[cfg(feature = "audio")] {
 		let a2t_pipe = A2T_PIPE.read();
 		if let Some(a2t_pipe) = &*a2t_pipe {
@@ -472,11 +649,11 @@ pub extern "C" fn is_talking() -> bool {
 }
 
 #[no_mangle]
-pub extern "C" fn set_talking(talking: bool) {
+pub extern "C" fn tscl_set_talking(_talking: bool) {
 	#[cfg(feature = "audio")] {
 		let a2t_pipe = A2T_PIPE.read();
 		if let Some(a2t_pipe) = &*a2t_pipe {
-			if let Err(e) = a2t_pipe.set_playing(talking) {
+			if let Err(e) = a2t_pipe.set_playing(_talking) {
 				error!(LOGGER, "Failed to set talking state"; "error" => ?e);
 			}
 		}
@@ -484,31 +661,10 @@ pub extern "C" fn set_talking(talking: bool) {
 }
 
 #[no_mangle]
-pub extern "C" fn next_event(ev: *mut FfiEvent) {
+pub extern "C" fn tscl_next_event(ev: *mut FfiEvent) {
 	let event = EVENTS.1.recv().unwrap();
 	unsafe {
-		let typ = event.get_type();
-		*ev = FfiEvent {
-			content: match event {
-				Event::ConnectionAdded(c) => FfiEventUnion {
-					connection_added: c,
-				},
-				Event::ConnectionRemoved(c) => FfiEventUnion {
-					connection_removed: c,
-				},
-				Event::Event(id, LibEvent::Message { from, invoker, message }) => FfiEventUnion {
-					message: EventMsg {
-						connection: id,
-						target_type: from.into(),
-						// TODO Send all invoker data instead of id
-						invoker: invoker.id.0,
-						message: CString::new(message).unwrap().into_raw(),
-					}
-				},
-				Event::Event(_, _) => unimplemented!("Events apart from message are not yet implemented"),
-			},
-			typ,
-		}
+		*ev = event.into();
 	};
 }
 
@@ -516,12 +672,24 @@ pub extern "C" fn next_event(ev: *mut FfiEvent) {
 ///
 /// For the targets `Server` and `Channel`, the `target` parameter is ignored.
 #[no_mangle]
-pub extern "C" fn send_message(con_id: ConnectionId, target_type: MessageTarget, target: u16, msg: *const c_char) {
+pub unsafe extern "C" fn tscl_send_message(
+	con_id: ConnectionId,
+	target_type: MessageTarget,
+	target: u16,
+	msg: *const c_char,
+) -> FutureHandle {
+	let msg = ffi_to_str(&msg).unwrap();
+	send_message(con_id, target_type, target, msg).ffi()
+}
+
+fn send_message(
+	con_id: ConnectionId,
+	target_type: MessageTarget,
+	target: u16,
+	msg: &str,
+) -> BoxFuture<()> {
 	use tsclientlib::MessageTarget as MT;
 
-	// TODO Returns future
-	let msg = unsafe { CStr::from_ptr(msg) };
-	let msg = msg.to_str().unwrap();
 	if let Some(con) = CONNECTIONS.get(&con_id) {
 		let target = match target_type {
 			MessageTarget::Server => MT::Server,
@@ -529,27 +697,30 @@ pub extern "C" fn send_message(con_id: ConnectionId, target_type: MessageTarget,
 			MessageTarget::Client => MT::Client(ClientId(target)),
 			MessageTarget::Poke => MT::Poke(ClientId(target)),
 		};
-		RUNTIME.executor().spawn(con.lock().to_mut().send_message(target, &msg)
-			.map_err(|e| error!(LOGGER, "Failed to send message"; "error" => ?e)));
+		Box::new(con.lock().to_mut().send_message(target, msg).from_err())
 	} else {
-		error!(LOGGER, "Connection not found"; "function" => "send_message");
+		Box::new(future::err(format_err!("Connection not found").into()))
 	}
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn free_str(s: *mut c_char) { CString::from_raw(s); }
+pub unsafe extern "C" fn tscl_free_str(s: *mut c_char) {
+	if !s.is_null() {
+		CString::from_raw(s);
+	}
+}
 
 #[no_mangle]
-pub unsafe extern "C" fn free_u64s(ptr: *mut u64, len: usize) {
+pub unsafe extern "C" fn tscl_free_u64s(ptr: *mut u64, len: usize) {
 	Box::from_raw(std::slice::from_raw_parts_mut(ptr, len));
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn free_u16s(ptr: *mut u16, len: usize) {
+pub unsafe extern "C" fn tscl_free_u16s(ptr: *mut u16, len: usize) {
 	Box::from_raw(std::slice::from_raw_parts_mut(ptr, len));
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn free_char_ptrs(ptr: *mut *mut c_char, len: usize) {
+pub unsafe extern "C" fn tscl_free_char_ptrs(ptr: *mut *mut c_char, len: usize) {
 	Box::from_raw(std::slice::from_raw_parts_mut(ptr, len));
 }
