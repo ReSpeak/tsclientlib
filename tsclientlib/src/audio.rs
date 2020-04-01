7ffc59f360f35605dcfeaff3beea7f94575b6bb0
Flakebi <flakebi@t-online.de>
2020-04-01 23:15:37 +0200

    Fix some audio bugs and add tests

--- tsclientlib/src/audio.rs
+++ tsclientlib/src/audio.rs
@@ -24,7 +24,7 @@ const CHANNEL_NUM: usize = 2;
 /// If this amount of packets is lost consecutively, we assume the stream stopped.
 const MAX_PACKET_LOSSES: usize = 3;
 /// Store the buffer sizes for the last `LAST_BUFFER_SIZE_COUNT` packets.
-const LAST_BUFFER_SIZE_COUNT: u16 = 256;
+const LAST_BUFFER_SIZE_COUNT: u16 = 512;
 /// The amount of samples to maximally buffer. Equivalent to 0.5 s.
 const MAX_BUFFER_SIZE: usize = 48_000 / 2;
 /// Maximum number of packets in the queue.
@@ -33,6 +33,24 @@ const MAX_BUFFER_PACKETS: usize = 50;
 const MAX_BUFFER_TIME: usize = 48_000 / 2;
 /// Duplicate or remove every `step` sample when speeding-up.
 const SPEED_CHANGE_STEPS: usize = 100;
+
+#[derive(Clone, Debug)]
+struct SlidingWindowMinimum {
+	/// How long a value stays in the sliding window.
+	size: u16,
+	/// This is a sliding window minimum, it contains
+	/// `(insertion time, value)`.
+	///
+	/// When we insert a value, we can remove all bigger sample counts,
+	/// thus the queue always stays sorted with the minimum at the front
+	/// and the maximum at the back (latest entry).
+	///
+	/// Provides amortized O(1) minimum.
+	/// Source: https://people.cs.uct.ac.za/~ksmith/articles/sliding_window_minimum.html#sliding-window-minimum-algorithm
+	queue: VecDeque<(u16, usize)>,
+	/// The current insertion time.
+	cur_time: u16,
+}
 
 struct QueuePacket {
 	packet: InAudioBuf,
@@ -64,20 +82,8 @@ pub struct AudioQueue {
 	buffering_samples: usize,
 	/// The amount of samples in the buffer when a packet was decoded.
 	///
-	/// This is a sliding window minimum, it contains
-	/// `(insertion time, samples in buffer)`.
-	///
-	/// When we insert a sample count, we can remove all bigger sample counts,
-	/// thus the queue always stays sorted with the minimum at the front
-	/// (longest time in the queue) and the maximum at the back (latest entry).
-	///
-	/// Provides amortized O(1) minimum.
-	/// Source: https://people.cs.uct.ac.za/~ksmith/articles/sliding_window_minimum.html#sliding-window-minimum-algorithm
-	///
 	/// Used to expand or reduce the buffer.
-	last_buffer_samples: VecDeque<(u16, usize)>,
-	/// The current insertion time for `last_buffer_samples`.
-	cur_last_buffer_sample: u16,
+	last_buffer_samples: SlidingWindowMinimum,
 	/// Buffered for this duration.
 	buffered_for_samples: usize,
 }
@@ -93,6 +99,28 @@ pub struct AudioHandler<Id: Clone + Eq + Hash + PartialEq = ClientId> {
 	///
 	/// Updated when a new queue gets added.
 	avg_buffer_samples: usize,
+}
+
+impl SlidingWindowMinimum {
+	fn new(size: u16) -> Self {
+		Self { size, queue: Default::default(), cur_time: 0 }
+	}
+
+	fn push(&mut self, value: usize) {
+		while self.queue.back().map(|(_, s)| *s >= value).unwrap_or_default() {
+			self.queue.pop_back();
+		}
+		let i = self.cur_time;
+		self.queue.push_back((i, value));
+		self.cur_time = self.cur_time.wrapping_add(1);
+		while self.queue.front().map(|(i, _)| self.cur_time.wrapping_sub(*i) > self.size).unwrap_or_default() {
+			self.queue.pop_front();
+		}
+	}
+
+	fn get_min(&self) -> usize {
+		self.queue.front().map(|(_, s)| *s).unwrap_or_default()
+	}
 }
 
 impl AudioQueue {
@@ -111,8 +139,7 @@ impl AudioQueue {
 			last_packet_samples,
 			packet_loss_num: 0,
 			buffering_samples: 0,
-			last_buffer_samples: Default::default(),
-			cur_last_buffer_sample: 0,
+			last_buffer_samples: SlidingWindowMinimum::new(LAST_BUFFER_SIZE_COUNT),
 			buffered_for_samples: 0,
 		};
 		res.add_buffer_size(0);
@@ -125,41 +152,39 @@ impl AudioQueue {
 
 	/// Size is in samples.
 	fn add_buffer_size(&mut self, size: usize) {
-		while self.last_buffer_samples.back().map(|(_, s)| *s >= size).unwrap_or_default() {
-			self.last_buffer_samples.pop_back();
-		}
-		let i = self.cur_last_buffer_sample;
-		self.last_buffer_samples.push_back((i, size));
-		self.cur_last_buffer_sample += 1;
-		while self.last_buffer_samples.front().map(|(i, _)| self.cur_last_buffer_sample.wrapping_sub(*i) > LAST_BUFFER_SIZE_COUNT).unwrap_or_default() {
-			self.last_buffer_samples.pop_front();
-		}
+		self.last_buffer_samples.push(size);
 	}
 
 	fn get_min_queue_size(&self) -> usize {
-		self.last_packet_samples + self.last_buffer_samples.front()
-			.map(|(_, s)| *s).unwrap_or_default()
+		self.last_packet_samples + self.last_buffer_samples.get_min()
 	}
 
 	fn add_packet(&mut self, logger: &Logger, packet: InAudioBuf) -> Result<()> {
 		if self.packet_buffer.len() >= MAX_BUFFER_PACKETS {
 			bail!("Audio queue is full, dropping");
 		}
-		let samples = packet::nb_samples(packet.data().data().data(), SAMPLE_RATE)?;
+		let samples;
+		if packet.data().data().data().is_empty() {
+			// End of stream
+			samples = 0;
+		} else {
+			samples = packet::nb_samples(packet.data().data().data(), SAMPLE_RATE)?;
+		}
+
 		let id = packet.data().data().id();
 		let packet = QueuePacket {
 			packet,
 			samples,
 			id,
 		};
-		if usize::from(id.wrapping_sub(self.next_id)) > MAX_BUFFER_PACKETS {
+		if id.wrapping_sub(self.next_id) > MAX_BUFFER_PACKETS as u16 {
 			bail!("Audio packet is too late, dropping");
 		}
 		// Put into first spot where the id is smaller
 		let i = self.packet_buffer.len() - self.packet_buffer.iter().enumerate()
-			.rev().take_while(|(_, p)| id < p.id).count();
+			.rev().take_while(|(_, p)| p.id.wrapping_sub(id) <= MAX_BUFFER_PACKETS as u16).count();
 		trace!(logger, "Insert packet {} at {}", id, i);
-		let last_id = self.packet_buffer.back().map(|p| p.id + 1).unwrap_or(id);
+		let last_id = self.packet_buffer.back().map(|p| p.id.wrapping_add(1)).unwrap_or(id);
 		if last_id <= id {
 			self.buffering_samples = self.buffering_samples.saturating_sub(samples);
 			// Reduce buffering counter by lost packets if there are some
@@ -209,7 +234,8 @@ impl AudioQueue {
 			trace!(logger, "Ids"; "last_id" => last.id,
 				"next_id" => self.next_id,
 				"first_id" => self.packet_buffer.front().unwrap().id,
-				"buffer_len" => self.packet_buffer.len());
+				"buffer_len" => self.packet_buffer.len(),
+				"min_size" => self.get_min_queue_size());
 			count += (usize::from(last.id.wrapping_sub(self.next_id))
 				+ 1 - self.packet_buffer.len()) * self.last_packet_samples;
 		}
@@ -219,7 +245,10 @@ impl AudioQueue {
 	}
 
 	/// Decode data and return the requested length of buffered data.
-	pub fn get_next_data(&mut self, logger: &Logger, len: usize) -> Result<&[f32]> {
+	///
+	/// Returns `true` in the second return value when the stream ended,
+	/// `false` when it continues normally.
+	pub fn get_next_data(&mut self, logger: &Logger, len: usize) -> Result<(&[f32], bool)> {
 		if self.buffering_samples > 0 {
 			if self.buffered_for_samples >= MAX_BUFFER_TIME {
 				self.buffering_samples = 0;
@@ -232,7 +261,7 @@ impl AudioQueue {
 				trace!(logger, "Buffering";
 					"buffered_for_samples" => self.buffered_for_samples,
 					"buffering_samples" => self.buffering_samples);
-				return Ok(&[]);
+				return Ok((&[], false));
 			}
 		}
 
@@ -250,6 +279,10 @@ impl AudioQueue {
 
 			// Decode a packet
 			if let Some(packet) = self.packet_buffer.pop_front() {
+				if packet.packet.data().data().data().is_empty() {
+					return Ok((&self.decoded_buffer[self.decoded_pos..], true));
+				}
+
 				self.packet_buffer_samples -= packet.samples;
 				let cur_id = self.next_id;
 				self.next_id = self.next_id.wrapping_add(1);
@@ -266,10 +299,11 @@ impl AudioQueue {
 					self.packet_buffer_samples += packet.samples;
 					self.packet_buffer.push_front(packet);
 				} else {
-					debug_assert!(packet.id == cur_id, "Invalid packet queue state");
+					debug_assert!(packet.id == cur_id, "Invalid packet queue state: {} != {}", packet.id, cur_id);
 					self.decode_packet(logger, Some(&packet), false)?;
 				}
 			} else {
+				// TODO This happens too often, we should buffer more if this happens
 				debug!(logger, "No packets in queue");
 				// Packet loss or end of stream
 				self.decode_packet(logger, None, false)?;
@@ -293,7 +327,7 @@ impl AudioQueue {
 				if let Some(p) = self.packet_buffer.front() {
 					self.next_id = p.id;
 				}
-			} else if min > self.last_packet_samples {
+			} else if min_left > self.last_packet_samples {
 				// Speed-up
 				debug!(logger, "Speed-up buffer"; "min" => min,
 					"cur_packet_count" => self.packet_buffer.len(),
@@ -308,7 +342,7 @@ impl AudioQueue {
 
 		let res = &self.decoded_buffer[self.decoded_pos..(self.decoded_pos + len)];
 		self.decoded_pos += len;
-		Ok(res)
+		Ok((res, false))
 	}
 }
 
@@ -355,9 +389,12 @@ impl<Id: Clone + Eq + Hash + PartialEq> AudioHandler<Id> {
 					warn!(self.logger, "Failed to decode audio packet";
 						"error" => ?e);
 				}
-				Ok(r) => {
+				Ok((r, is_end)) => {
 					for i in 0..r.len() {
 						buf[i] += r[i];
+					}
+					if is_end {
+						to_remove.push(id.clone());
 					}
 				}
 			}
@@ -377,14 +414,7 @@ impl<Id: Clone + Eq + Hash + PartialEq> AudioHandler<Id> {
 		}
 
 		if let Some(queue) = self.queues.get_mut(&id) {
-			if empty {
-				// TODO Only remove when decoding and the previous packets have been played
-				trace!(self.logger, "Removing talker");
-				self.queues.remove(&id);
-				self.talkers_changed = true;
-			} else {
-				queue.add_packet(&self.logger, packet)?;
-			}
+			queue.add_packet(&self.logger, packet)?;
 		} else {
 			if empty {
 				return Ok(());
@@ -402,5 +432,61 @@ impl<Id: Clone + Eq + Hash + PartialEq> AudioHandler<Id> {
 			self.talkers_changed = true;
 		}
 		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use std::sync::Mutex;
+
+	use slog::{o, Drain};
+	use tsproto_packets::packets::{Direction, OutAudio};
+
+	use super::*;
+
+	fn create_logger() -> Logger {
+		let decorator =
+			slog_term::PlainDecorator::new(slog_term::TestStdoutWriter);
+		let drain = Mutex::new(slog_term::FullFormat::new(decorator).build())
+				.fuse();
+
+		slog::Logger::root(drain, o!())
+	}
+
+	#[test]
+	fn sliding_window_minimum() {
+		let data = &[(5, 5), (6, 5), (3, 3), (4, 3), (6, 3), (5, 3), (6, 3),
+			(6, 4), (6, 5), (6, 5), (6, 6), (7, 6), (5, 5)];
+		let mut window = SlidingWindowMinimum::new(5);
+		assert_eq!(window.get_min(), 0);
+		for (i, (val, min)) in data.iter().enumerate() {
+			println!("{:?}", window);
+			window.push(*val);
+			assert_eq!(window.get_min(), *min, "Failed in iteration {} ({:?})",
+				i, window);
+		}
+	}
+
+	#[test]
+	fn packets_wrapping() {
+		let logger = create_logger();
+		let mut handler = AudioHandler::<ClientId>::new(logger);
+		let id = ClientId(0);
+		let mut buf = vec![0.0; 48_000 / 100 * 2];
+
+		for i in 0..100 {
+			let packet = OutAudio::new(&AudioData::S2C {
+				id: 65_500u16.wrapping_add(i),
+				codec: CodecType::OpusMusic,
+				from: 0,
+				data: &[0, 0, 0, 0, 0, 0, 0],
+			});
+			let input = InAudioBuf::try_new(Direction::S2C, packet.into_vec()).unwrap();
+			handler.handle_packet(id, input).unwrap();
+
+			if i > 5 {
+				handler.fill_buffer(&mut buf);
+			}
+		}
 	}
 }
